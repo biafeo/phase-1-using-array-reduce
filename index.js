920ed65ae21a6675bb5305d4df2be7d05875f26a
beatriz <beatrizffeo@gmail.com>
2024-03-18 19:16:56 -0600

    batteries adding

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(function(sum, currentBatteries) {
+  return sum + currentBatteries;
+}, 0);
+
 // Code your solution here
+// const totalBatteries = batteryBatches.reduce(function(countObj, currentBatteries) {
+//     if (countObj.hasOwnProperty(currentBatteries)) {
+//       countObj[currentBatteries]++;
+//     } else {
+//       countObj[currentBatteries] = 1;
+//     }
+//     return countObj;
+//   }, {});
+//   const totalBatteries = Object.entries(batteryBatches).reduce(function(sum, [count, occurrences]) {
+//     return sum + (count * occurrences);
+//   }, 0);
